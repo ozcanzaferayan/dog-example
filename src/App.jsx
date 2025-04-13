60cbef7e4f821575470c83278bdd7aeadd1ebbaa
Özcan Zafer AYAN <ozcanzaferayan@gmail.com>
2025-04-13 12:50:33 +0300

    Proje eklendi

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  // usss
+  // array dönüyorsa boş array atıyorum.
+  // object dönüyorsa null atıyorum.
+  const [images, setImages] = useState([]);
+
+  // uffs
+  useEffect(() => {
+    const API_URL = "https://dog.ceo/api/breed/chihuahua/images";
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((veri) => setImages(veri.message));
+  }, []);
+
+  return (
+    <div>
+      {images.map((imageUrl) => (
+        <img src={imageUrl} width={100} height={100} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
